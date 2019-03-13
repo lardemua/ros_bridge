06e211a21f558b631e54d074f6c1ed94baaeab72
Pedro Silva <silva.mfpedro@gmail.com>
2019-03-13 23:07:19 +0000

    Changes in vehicle files

--- src/carla_ros_bridge/ego_vehicle.py
+++ src/carla_ros_bridge/ego_vehicle.py
@@ -14,3 +14,19 @@ Classes to handle Carla vehicles
 # ------------------------
 #   IMPORTS
 # ------------------------
+import sys
+import datetime
+import numpy
+from simple_pid import PID
+import rospy
+from dynamic_reconfigure.server import Server
+from nav_msgs.msg import Odometry
+from std_msgs.msg import ColorRGBA
+from ackermann_msgs.msg import AckermannDrive
+from carla import VehicleControl
+from carla_ros_bridge.vehicle import Vehicle
+import carla_ros_bridge.physics as phys
+from carla_ros_bridge.msg import CarlaVehicleControl                # pylint: disable=no-name-in-module,import-error
+from carla_ros_bridge.msg import EgoVehicleControlInfo              # pylint: disable=no-name-in-module,import-error
+from carla_ros_bridge.cfg import EgoVehicleControlParameterConfig   # pylint: disable=no-name-in-module,import-error
+
