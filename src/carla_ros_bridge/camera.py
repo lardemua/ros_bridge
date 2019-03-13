97f4c8feb2f85b956abad85137eacc2eaa38263c
Pedro Silva <silva.mfpedro@gmail.com>
2019-03-13 22:32:11 +0000

    Changes in camera.py and sensor.py files

--- src/carla_ros_bridge/camera.py
+++ src/carla_ros_bridge/camera.py
@@ -24,3 +24,287 @@ from sensor_msgs.msg import CameraInfo
 import carla
 from carla_ros_bridge.sensor import Sensor
 import carla_ros_bridge.transforms as trans
+
+
+class Camera(Sensor):
+    """
+    Sensor Implementation details for the Camera objects
+    """
+
+    # global cv bridge to convert image between OpenCV and ROS
+    cv_bridge = CvBridge()
+
+    @staticmethod
+    def create_actor(carla_actor, parent):
+        """
+        Static Factory Method to create camera actors
+        :param carla_actor: Carla Camera Actor Object
+        :type carla_actor: carla.Camera
+        :param parent: Parent of new Traffic Actor
+        :type parent: carla_ros_bridge.Parent
+        :return: Created Camera Actor
+        :rtype: carla_ros_bridge.Camera or derived type
+        """
+        if carla_actor.type_id.startswith("sensor.camera.rgb"):
+            return RGB_Camera(carla_actor=carla_actor, parent=parent)
+        elif carla_actor.type_id.startswith("sensor.camera.depth"):
+            return Depth_Camera(carla_actor=carla_actor, parent=parent)
+        elif carla_actor.type_id.startswith("sensor.camera.semantic_segmentation"):
+            return Semantic_Segmentation_Camera(carla_actor=carla_actor, parent=parent)
+        else:
+            return Camera(carla_actor=carla_actor, parent=parent)
+
+    def __init__(self, carla_actor, parent, topic_prefix=True):
+        """
+        Constructor for Camera Class
+        :param carla_actor: Carla Actor Object
+        :type carla_actor: carla.Actor
+        :param parent: Parent of this Actor Object
+        :param topic_prefix: The Topic Prefix to be used for this actor
+        :type topic_prefix: string
+        """
+        if topic_prefix is None:
+            topic_prefix = 'camera'
+            super(Camera, self).__init__(carla_actor=carla_actor, parent=parent, topic_prefix=topic_prefix)
+        if self.__class__.__name__ == "Camera":
+            rospy.logwarn("Created Unsupported Camera Actor"
+                          "(id={}, parent_id={}, type={}, attributes={})".format(self.get_ID(), self.get_parent_ID(),
+                                                                                 self.carla_actor.type_id, self.carla_actor.attributes))
+        else:
+            self._build_camera_info()
+
+    def _build_camera_info(self):
+        """
+        Private Function used to compute camera info
+        Considers that the camera info does not change over time
+        :return:
+        """
+        camera_info = CameraInfo()
+        # store camera info without the header
+        camera_info.header = None
+        camera_info.width = int(self.carla_actor.attributes['image_size_x'])
+        camera_info.height = int(self.carla_actor.attributes['image_size_y'])
+        camera_info.distortion_model = 'plumb_bob'
+        cx = camera_info.width / 2.0
+        cy = camera_info.height / 2.0
+        fx = camera_info.width / (2.0 * math.tan(float(self.carla_actor.attributes['fov']) * math.pi / 360.0))
+        fy = fx
+        camera_info.K = [fx, 0, cx, 0, fy, cy, 0, 0, 1]
+        camera_info.D = [0, 0, 0, 0, 0]
+        camera_info.R = [1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0]
+        camera_info.P = [fx, 0, cx, 0, 0, fy, cy, 0, 0, 0, 1.0, 0]
+        self._camera_info = camera_info
+
+    def sensor_data_updated(self, carla_image):
+        """
+        Override Function used to transform the received carla image data into a ROS image message
+        :param carla_image: Carla Image Object
+        :type carla_image: carla.Image
+        :return:
+        """
+        if (carla_image.height != self._camera_info.height) or (carla_image.width != self._camera_info.width):
+            rospy.logerr("Camera{} received image not matching configuration".format(self.topic_name()))
+
+        image_data_array, encoding = self.get_carla_image_data_array(carla_image=carla_image)
+        img_msg = Camera.cv_bridge.cv2_to_imgmsg(image_data_array, encoding=encoding)
+        # the camera data is in respect to the camera's own frame
+        img_msg.header = self.get_msg_header(use_parent_frame=False)
+
+        cam_info = self._camera_info
+        cam_info.header = img_msg.header
+
+        self.publish_ros_message(self.topic_name() + '/camera_info', cam_info)
+        self.publish_ros_message(self.topic_name() + '/' + self.get_image_topic_name(), img_msg)
+
+    def get_tf_msg(self):
+        """
+        Override Function used to modify the tf messages sent by this camera.
+        The camera transformation has to bve altered to look at the same axis as the OpenCV projection in order to get
+        easy depth cloud for RGBD cameras.
+        :return: Filled tf Message
+        :rtype: geometry_msgs.msg.TransformStamped
+        """
+        tf_msg = super(Camera, self).get_tf_msg()
+        rotation = tf_msg.transform.rotation
+        quaternion = [rotation.x, rotation.y, rotation.z, rotation.w]
+        quaternion_swap = tf.transformations.quaternion_from_matrix([[0, 0, 1, 0], [-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]])
+        quaternion = tf.transformations.quaternion_multiply(quaternion, quaternion_swap)
+
+        tf_msg.transform.rotation = trans.numpy_quaternion_to_ros_quaternion(quaternion)
+        return tf_msg
+
+    @abstractmethod
+    def get_carla_image_data_array(self, carla_image):
+        """
+        Virtual Function used to convert the Carla Image to a numpy data array
+        as input for the cv_bridge.cv2_to_imgmsg() function
+        :param carla_image: Carla Image Object
+        :type carla_image: carla.Image
+        :return: Tuple (numpy data array containing the image transformation, encoding)
+        :rtype: Tuple (numpy.ndarray, string)
+        """
+        raise NotImplementedError("This function has to be re-implemented by derived classes")
+
+    @abstractmethod
+    def get_image_topic_name(self):
+        """
+        Virtual Function used to provide the actual image topic name
+        :return: Image Topic Name
+        :rtype: string
+        """
+        raise NotImplementedError("This function has to be re-implemented by derived classes")
+
+
+class RGB_Camera(Camera):
+    """
+       Camera Implementation Details for RGB Camera
+    """
+
+    def __init__(self, carla_actor, parent, topic_prefix=None):
+        """
+        Constructor of the RGB_Camera object
+        :param carla_actor: Carla Actor Object
+        :type carla_actor: carla.Actor
+        :param parent: Parent of this Actor Object Node
+        :type parent: carla_ros_bridge.Parent
+        :param topic_prefix: Topic prefix to be used for this actor
+        :type topic_prefix: string
+        """
+        if topic_prefix is None:
+            topic_prefix = 'camera/rgb'
+        super(RGB_Camera, self).__init__(carla_actor=carla_actor, parent=parent, topic_prefix=topic_prefix)
+
+    def get_carla_image_data_array(self, carla_image):
+        """
+        Override Function used to convert the Carla Image to a numpy data array
+        as input for the cv_bridge.cv2_to_imgmsg() function
+        The RGB Camera provides a 4-channel int8 color format(BGRA).
+        :param carla_image: Carla Image Object
+        :type carla_image: carla.Image
+        :return: Tuple (numpy data array containing the image information, encoding)
+        :rtype: Tuple (numpy.ndarray, string)
+        """
+        carla_image_data_array = numpy.ndarray(shape=(carla_image.height, carla_image.width, 4),
+                                               dtype=numpy.uint8,
+                                               buffer=carla_image.raw_data)
+        return carla_image_data_array, 'bgra8'
+
+    def get_image_topic_name(self):
+        """
+        Virtual Function used to provide the actual image topic name
+        :return: Image Topic Name
+        :rtype: string
+        """
+        return "image_color"
+
+
+class Depth_Camera(Camera):
+    """
+    Camera Implementation Details for Depth Camera
+    """
+
+    def __init__(self, carla_actor, parent, topic_prefix=None):
+        """
+        Constructor of the Depth Camera Object
+        :param carla_actor: Carla Actor Object
+        :type carla_actor: carla.Actor
+        :param parent: Parent of this Actor Object Node
+        :type parent: carla_ros_bridge.Parent
+        :param topic_prefix: Topic prefix to be used for this actor
+        :type topic_prefix: string
+        """
+        if topic_prefix is None:
+            topic_prefix = 'camera/depth'
+        super(Depth_Camera, self).__init__(carla_actor=carla_actor, parent=parent, topic_prefix=topic_prefix)
+
+    def get_carla_image_data_array(self, carla_image):
+        """
+        Override Function used to convert the Carla Image to a numpy data array
+        as input for the cv_bridge.cv2_to_imgmsg() function
+        The RGB Camera provides a 4-channel int8 color format(BGRA).
+        :param carla_image: Carla Image Object
+        :type carla_image: carla.Image
+        :return: Tuple (numpy data array containing the image information, encoding)
+        :rtype: Tuple (numpy.ndarray, string)
+        """
+        # color conversion within C++ code is broken, when transforming a
+        #  4-channel uint8 color pixel into a 1-channel float32 grayscale pixel
+        # therefore, we do it on our own here
+        #
+        # @todo: After fixing https://github.com/carla-simulator/carla/issues/1041
+        # the final code in here should look like:
+        #
+        # carla_image.convert(carla.ColorConverter.Depth)
+        #
+        # carla_image_data_array = numpy.ndarray(
+        #    shape=(carla_image.height, carla_image.width, 1),
+        #    dtype=numpy.float32, buffer=carla_image.raw_data)
+        #
+        bgra_image = numpy.ndarray(shape=(carla_image.height, carla_image.width, 4),
+                                   dtype=numpy.uint8,
+                                   buffer=carla_image.raw_data)
+
+        # Apply (R + G * 256 + B * 256 * 256) / (256**3 - 1) * 1000
+        # according to the documentation:
+        # https://carla.readthedocs.io/en/latest/cameras_and_sensors/#camera-depth-map
+        scales = numpy.array([65536.0, 256.0, 1.0, 0]) / (256 ** 3 - 1) * 1000
+        depth_image = numpy.dot(bgra_image, scales).astype(numpy.float32)
+
+        # actually we want encoding '32FC1'
+        # which is automatically selected by cv bridge with passthrough
+        return depth_image, 'passthrough'
+
+    def get_image_topic_name(self):
+        """
+        Virtual Function used to provide the actual image topic name
+        :return: Image Topic Name
+        :rtype: string
+        """
+        return "image_depth"
+
+
+class Semantic_Segmentation_Camera(Camera):
+    """
+        Camera Implementation Details for Semantic Segmentation Camera
+    """
+
+    def __init__(self, carla_actor, parent, topic_prefix=None):
+        """
+        Constructor of the Depth Camera Object
+        :param carla_actor: Carla Actor Object
+        :type carla_actor: carla.Actor
+        :param parent: Parent of this Actor Object Node
+        :type parent: carla_ros_bridge.Parent
+        :param topic_prefix: Topic prefix to be used for this actor
+        :type topic_prefix: string
+        """
+        if topic_prefix is None:
+            topic_prefix = 'camera/semantic_segmentation'
+        super(Semantic_Segmentation_Camera, self).__init__(carla_actor=carla_actor, parent=parent, topic_prefix=topic_prefix)
+
+    def get_carla_image_data_array(self, carla_image):
+        """
+        Override Function used to convert the Carla Image to a numpy data array
+        as input for the cv_bridge.cv2_to_imgmsg() function
+        The RGB Camera provides a 4-channel int8 color format(BGRA).
+        :param carla_image: Carla Image Object
+        :type carla_image: carla.Image
+        :return: Tuple (numpy data array containing the image information, encoding)
+        :rtype: Tuple (numpy.ndarray, string)
+        """
+        carla_image.convert(carla.ColorConverter.CityScapesPalette)
+        carla_image_data_array = numpy.ndarray(shape=(carla_image.height, carla_image.width, 4),
+                                               dtype=numpy.uint8,
+                                               buffer=carla_image.raw_data)
+        return carla_image_data_array, 'bgra8'
+
+    def get_image_topic_name(self):
+        """
+        Virtual Function used to provide the actual image topic name
+        :return: Image Topic Name
+        :rtype: string
+        """
+        return "image_segmentation"
+
+
+
--- src/carla_ros_bridge/sensor.py
+++ src/carla_ros_bridge/sensor.py
@@ -21,6 +21,7 @@ from geometry_msgs.msg import TransformStamped
 from carla_ros_bridge.actor import Actor
 import carla_ros_bridge.transforms as trans
 
+
 class Sensor(Actor):
     """
     Actor Implementation Details for Sensors
@@ -37,3 +38,113 @@ class Sensor(Actor):
         :return: Created sensor Actor
         :rtype: carla_ros_bridge.Sensor or derived type.
         """
+        if carla_actor.type_id.startswith("sensor.camera"):
+            return Camera.create_actor(carla_actor=carla_actor, parent=parent)
+        if carla_actor.type_id.startswith("sensor.lidar"):
+            return Lidar(carla_actor=carla_actor, parent=parent)
+        if carla_actor.type_id.startswith("sensor.other.gnss"):
+            return Gnss(carla_actor=carla_actor, parent=parent)
+        else:
+            return Sensor(carla_actor=carla_actor, parent=parent)
+
+    def __init__(self, carla_actor, parent, topic_prefix=None, append_role_name_topic_postfix=True):
+        """
+        Constructor for Sensor Class
+        :param carla_actor: Carla Actor Object
+        :type carla_actor: carla.Actor
+        :param parent: Parent of current node
+        :type parent: carla_ros_bridge.Parent
+        :param topic_prefix: Topic prefix to be used for this actor
+        :type topic_prefix: string
+        :param append_role_name_topic_postfix: if this flag is set True, the role_name of the actor is used as topic postfix
+        :type append_role_name_topic_postfix: boolean
+        """
+        if topic_prefix is None:
+            topic_prefix = 'sensor'
+            super(Sensor, self).__init__(carla_actor=carla_actor, parent=parent, topic_prefix=topic_prefix, append_role_name_topic_postfix=append_role_name_topic_postfix)
+            self.current_sensor_data = None
+            self.update_lock = threading.Lock()
+            if self.__class__.__name__ == "Sensor":
+                rospy.logwarn("Created Unsupported Sensor(id={}, parent_id={}"
+                              ", type={}, attributes={}".format(self.get_ID(), self.get_parent_ID(), self.carla_actor.type_id, self.carla_actor.attributes))
+            else:
+                self.carla_actor.listen(self._callback_sensor_data)
+
+    def destroy(self):
+        """
+        Override Function used to destroy this object.
+        Stop listening to the carla.Sensor actor.
+        Finally forward call to super class.
+        :return:
+        """
+        rospy.logdebug("Destroy Sensor(id={})".format(self.get_ID()))
+        if self.carla_actor.is_listening:
+            self.carla_actor.stop()
+        if self.update_lock.acquire():
+            self.current_sensor_data = None
+        super(Sensor, self).destroy()
+
+    def get_frame_ID(self):
+        """
+        Override Function used to get the frame ID of the sensor object.
+        Sensor frames respect their respective parent relationship within the frame name to prevent from name clashes.
+        :return: Frame ID of the Sensor Object
+        :rtype: string
+        """
+        return self.parent.get_frame_ID() + "/" + super(Sensor, self).get_frame_ID()
+
+    def _callback_sensor_data(self, carla_sensor_data):
+        """
+        Callback Function used to be called whenever new sensor data is received
+        :param carla_sensor_data: Carla Sensor Data Object
+        :type carla_sensor_data: carla.SensorData
+        :return:
+        """
+        if not rospy.is_shutdown():
+            if self.update_lock.acquire(False):
+                self.current_sensor_data = carla_sensor_data
+                self.send_tf_msg()
+                self.sensor_data_update(carla_sensor_data)
+                self.update_lock.release()
+
+    def get_tf_msg(self):
+        """
+        Override Function used to create a ROS tf message for this sensor
+        The reported transform of the sensor is in respect to the global frame.
+        :return: The Filled tf Message
+        :rtype: geometry_msgs.msg.TransformStamped
+        """
+        tf_msg = TransformStamped()
+        tf_msg.header = self.get_msg_header()
+        tf_msg.header.frame_id = "/map"
+        tf_msg.child_frame_id = self.get_frame_ID()
+        tf_msg.transform = self.get_current_ros_transform()
+        return tf_msg
+
+    def get_current_ros_transform(self):
+        """
+        Override Function used to provide the current ROS transform.
+        In general sensors are also actors, therefore they contain a transform that is updated within each tick.
+        But the TF beeing published should exactly match the transform received by SensorData.
+        :return: the ROS transform of this actor
+        :rtype: geometry_msgs.msg.Transform
+        """
+        return trans.carla_transform_to_ros_transform(self.current_sensor_data.transform)
+
+    @abstractmethod
+    def sensor_data_updated(self, carla_sensor_data):
+        """
+        Pure Virtual Function to transform the received carla sensor data
+        into a corresponding ROS message.
+        :param carla_sensor_data: Carla Sensor Data Object
+        :type carla_sensor_data: carla.SensorData
+        :return:
+        """
+        raise NotImplementedError("This function has to be implemented by the derived classes")
+
+# These imports have to be at the end to resolve cyclic dependency
+
+
+from carla_ros_bridge.camera import Camera       # noqa, pylint: disable=wrong-import-position
+from carla_ros_bridge.lidar import Lidar         # noqa, pylint: disable=wrong-import-position
+from carla_ros_bridge.gnss import Gnss           # noqa, pylint: disable=wrong-import-position
