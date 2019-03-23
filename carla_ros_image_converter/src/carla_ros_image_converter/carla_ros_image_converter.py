ceba61780301f3075d09393fd20c47d81d12c72a
Pedro Silva <silva.mfpedro@gmail.com>
2019-03-23 13:15:51 +0000

    Changes in the imports of carla_ros_image_converter.py

--- carla_ros_image_converter/src/carla_ros_image_converter/carla_ros_image_converter.py
+++ carla_ros_image_converter/src/carla_ros_image_converter/carla_ros_image_converter.py
@@ -5,14 +5,14 @@
 # ------------------------
 from __future__ import print_function
 
-import roslib
-roslib.load_manifest('my_package')
 import sys
 import rospy
 import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import roslib
+roslib.load_manifest('my_package')
 
 
 class Image_Converter:
