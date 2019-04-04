c15dddf863e39c662dd3693b82686872f80cb2b8
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-04 17:11:46 +0100

    Added carla_ego_vehicle catkin package to project folder

---
+++ carla_ego_vehicle/src/carla_ego_vehicle/carla_ros_manual_control.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
