a1c42059c08788baee4489cf3d99fe9576619f49
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-05 23:34:18 +0100

    Added carla_waypoint_publisher catkin package

---
+++ carla_waypoint_publisher/setup.py
@@ -0,0 +1,12 @@
+# ------------------------
+#   IMPORTS
+# ------------------------
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['carla_waypoint_publisher'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
