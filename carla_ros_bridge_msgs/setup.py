dede1db5375d67a83c2805571eaa166d098edcc8
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-07 21:13:15 +0100

    Corrected EgoVehicleControlInfo message

--- carla_ros_bridge_msgs/setup.py
+++ carla_ros_bridge_msgs/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['carla_ros_bridge_msgs'],
-    package_dir={'': 'src'}
+    # package_dir={'': 'msg'}
 )
 
 setup(**d)
