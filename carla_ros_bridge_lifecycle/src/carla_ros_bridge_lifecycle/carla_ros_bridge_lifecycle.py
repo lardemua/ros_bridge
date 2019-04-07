301307e3f12c1f340b90468eafcffbe1d0f90088
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-07 19:54:07 +0100

    Added changes to carla_ros_bridge_lifecycle package

--- carla_ros_bridge_lifecycle/src/carla_ros_bridge_lifecycle/carla_ros_bridge_lifecycle.py
+++ carla_ros_bridge_lifecycle/src/carla_ros_bridge_lifecycle/carla_ros_bridge_lifecycle.py
@@ -22,7 +22,7 @@ import threading
 import datetime
 import rospy
 import roslaunch
-from carla_ros_bridge.msg import CarlaEgoVehicleControl     # pylint: disable=no-name-in-module,import-error
+from carla_ros_bridge_msgs.msg import CarlaEgoVehicleControl     # pylint: disable=no-name-in-module,import-error
 
 
 class RosBridgeLifeCycle(object):
