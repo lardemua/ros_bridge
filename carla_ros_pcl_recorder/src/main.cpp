60eb9442bb227054e99a5cfde53c953def0a773a
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-09 14:54:12 +0100

    Added carla_ros_pcl_recorder catkin package

---
+++ carla_ros_pcl_recorder/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT>.
+ */
+
+/* System Includes */
+#include <ros/ros.h>
+#include "PclRecorder.h"
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "pcl_recorder");
+    PclRecorder pclRecorder;
+    ros::spin();
+};
