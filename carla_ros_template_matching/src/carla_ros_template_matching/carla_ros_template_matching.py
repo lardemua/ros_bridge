07e71790befa39ea546e66e3271508af883883b7
Pedro Silva <silva.mfpedro@gmail.com>
2019-05-19 12:48:10 +0100

    Added change to carla_ros_template_matching module

--- carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_template_matching.py
+++ carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_template_matching.py
@@ -9,8 +9,7 @@ import sys
 import rospy
 import cv2
 import carla
-import json
-from std_msgs.msg import String
+import numpy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import roslib
@@ -46,58 +45,56 @@ class Template_Matching:
         # print blueprints
         # print(blueprints)
 
-        data = {}
-        data['vehicles'] = []
-        for vehicle in world.get_actors().filter('vehicle.*'):
-            # print(vehicle.bounding_box)
-            # Draw bounding box
-            transform = vehicle.get_transform()
-            bounding_box = vehicle.bounding_box
-            bounding_box.location += transform.location
-            world.debug.draw_box(bounding_box, transform.rotation)
-            data = self.write_json_dataset(data, vehicle, transform)
+        template_1 = cv2.imread('template01.png', cv2.IMREAD_GRAYSCALE)
+        tW1, tH1 = template_1.shape[::-1]
 
-        cv2.imshow("Image Window", cv_img)
-        cv2.waitKey(1)
+        template_2 = cv2.imread('template02.png', cv2.IMREAD_GRAYSCALE)
+        tW2, tH2 = template_2.shape[::-1]
+
+        template_3 = cv2.imread('template03.png', cv2.IMREAD_GRAYSCALE)
+        tW3, tH3 = template_3.shape[::-1]
+
+        gray_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+        hsv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+
+        threshold = 0.8
+
+        # define range of white color in HSV and change it according to your need.
+        sensitivity = 15
+        # lower_white = np.array([0,0,0], dtype=np.uint8)
+        # upper_white = np.array([0,0,255], dtype=np.uint8)
+        lower_white = numpy.array([0, 0, 255 - sensitivity])
+        upper_white = numpy.array([255, sensitivity, 255])
+
+        # threshold the HSV image to get only white colors
+        mask = cv2.inRange(hsv_img, lower_white, upper_white)
+        cv2.imshow('mask', mask)
+        res1 = cv2.matchTemplate(gray_img, template_1, cv2.TM_CCOEFF_NORMED)
+        loc1 = numpy.where(res1 >= threshold)
+        res2 = cv2.matchTemplate(gray_img, template_2, cv2.TM_CCOEFF_NORMED)
+        loc2 = numpy.where(res2 >= threshold)
+        res3 = cv2.matchTemplate(gray_img, template_3, cv2.TM_SQDIFF_NORMED)
+        min_value0, max_value0, min_loc0, max_loc0 = cv2.minMaxLoc(res3)
+        loc3 = numpy.where(res3 <= min_value0)
+
+        for pt in zip(*loc1[::-1]):
+            cv2.rectangle(cv_img, pt, (pt[0] + tW1, pt[1] + tH1), (0, 0, 255), 2)
+
+        for pt in zip(*loc2[::-1]):
+            cv2.rectangle(cv_img, pt, (pt[0] + tW2, pt[1] + tH2), (0, 255, 255), 3)
+
+        for pt in zip(*loc3[::-1]):
+            cv2.rectangle(cv_img, pt, (pt[0] + tW3, pt[1] + tH3), (255, 255, 255), 5)
+
+        cv2.imshow("Detected Window", cv_img)
+        # Press q on the keyboard to shutdown.
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            rospy.is_shutdown()
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_img, "bgr8"))
         except CvBridgeError as e:
             print(e)
-        # save json dataset
-        self.save_json_dataset(data)
-        self.parse_json_dataset()
-
-    def write_json_dataset(self, data, vehicle, transform):
-        data['vehicles'].append({
-            'model': str(vehicle.attributes['object_type']),
-            'x': str(transform.location.x),
-            'y': str(transform.location.y),
-            'z': str(transform.location.z),
-            'yaw': str(transform.rotation.yaw),
-        })
-        # data['vehicle'].append("\\n\\n")
-        # data['people'].append({
-        #     'name': 'Larry',
-        #     'website': 'google.com',
-        #     'from': 'Michigan'
-        # })
-        # data['people'].append({
-        #     'name': 'Tim',
-        #     'website': 'apple.com',
-        #     'from': 'Alabama'
-        # })
-        return data
-
-    def save_json_dataset(self, data):
-        with open('/home/pedro/catkin_ws/src/ros_bridge/datasets/data.json', 'w') as json_file:
-            json.dump(data, json_file)
-
-    def parse_json_dataset(self):
-        with open('/home/pedro/catkin_ws/src/ros_bridge/datasets/data.json', 'r') as json_file:
-            for row in json_file:
-                data = json.loads(row)
-                json.dumps(data, sort_keys=True, indent=2, separators=(',', ': '))
 
 
 def main(args):
