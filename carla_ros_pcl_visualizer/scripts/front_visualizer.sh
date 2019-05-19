de739059e06d66132f7dbbc64ad1f8bb77d45b99
Pedro Silva <silva.mfpedro@gmail.com>
2019-05-19 20:24:35 +0100

    Added bash script files for carla_ros_pcl_visualizer package

---
+++ carla_ros_pcl_visualizer/scripts/front_visualizer.sh
@@ -0,0 +1,11 @@
+# Move to front LIDAR cloud directory
+cd /home/pedro/catkin_ws/src/ros_bridge/pointclouds/front/
+
+# Concatenate point cloud files
+pcl_concatenate_ponts_pcd *.pcd
+
+# Filter duplicate results
+pcl_voxel_grid -leaf 0.1,0.1,0.1 output.pcd map.pcd
+
+# Show result
+pcl_viewer map.pcd
