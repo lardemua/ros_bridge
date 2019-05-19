18d38e2ed8564851f1f5272008c856879bfad71f
Pedro Silva <silva.mfpedro@gmail.com>
2019-05-19 21:23:25 +0100

    Added changes to bash script files of carla_ros_pcl_visualizer package

--- carla_ros_pcl_visualizer/scripts/right_visualizer.sh
+++ carla_ros_pcl_visualizer/scripts/right_visualizer.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
-# Move to front LIDAR cloud directory
+# Move to right LIDAR cloud directory
+echo "Moving to right LIDAR cloud directory"
+echo " "
 cd /home/pedro/catkin_ws/src/ros_bridge/pointclouds/right/
+sleep 5
+echo " "
 
 # Concatenate point cloud files
+echo "Concatenating point cloud files"
+echo " "
 pcl_concatenate_ponts_pcd *.pcd
+sleep 5
+echo " "
 
 # Filter duplicate results
+echo "Filtering duplicated results"
+echo " "
 pcl_voxel_grid -leaf 0.1,0.1,0.1 output.pcd map.pcd
+sleep 5
+echo " "
+
 
 # Show result
+echo "Showing Point Cloud Map Result for Right Lidar Sensor"
+echo " "
 pcl_viewer map.pcd
+sleep 5
+echo " "
