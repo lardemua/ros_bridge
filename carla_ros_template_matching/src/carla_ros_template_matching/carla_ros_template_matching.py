aa1fb965e64ff5ef369f7c0642fb744e6341862d
Pedro Silva <silva.mfpedro@gmail.com>
2019-05-22 11:53:02 +0100

    Added changes to carla_ros_template_matching module

--- carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_template_matching.py
+++ carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_template_matching.py
@@ -18,7 +18,7 @@ roslib.load_manifest('carla_ros_template_matching')
 
 class Template_Matching:
     """
-    Class used for converting ROS images to OpenCV images
+    Class used for converting ROS images to OpenCV images and apply Template Matching
     """
     def __init__(self):
         self.image_pub = rospy.Publisher("/carla/ego_vehicle/camera/rgb/front/template_matching", Image)
