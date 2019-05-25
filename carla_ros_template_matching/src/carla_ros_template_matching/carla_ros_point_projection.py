3a43820fc36ab40c67d1404961968b787777134b
Pedro Silva <silva.mfpedro@gmail.com>
2019-05-25 17:24:30 +0100

    Changes in point projection module to carla_ros_template_matching package

--- carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_point_projection.py
+++ carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_point_projection.py
@@ -31,9 +31,19 @@ class Point_Projection:
         self.tH = None
         self.cropping = False
         self.template_matching = False
+        self.project_points = False
         self.threshold = 0.8
         self.obj_points = []     # 3d points in real world space
         self.img_points = []     # 2d points in image plane
+        self.calibration = []
+        self.distCoeffs = []
+        self.rotVector = []
+        self.transVector = []
+        self.end_point_3d = []
+        self.end_point_2d =  []
+        self.view_width = 1920//2
+        self.view_height = 1080//2
+        self.view_fov = 90
 
     def shape_selection(self, event, x, y, flags, param):
         # if the left mouse button was clicked, record the starting (x,y) coordinates
@@ -63,12 +73,26 @@ class Point_Projection:
         world = client.get_world()
         blueprints = world.get_blueprint_library().filter('vehicle.*')
 
+        calibration = numpy.identity(3)
+        calibration[0, 2] = self.view_width/ 2.0
+        calibration[1, 2] = self.view_height / 2.0
+        calibration[0, 0] = calibration[1, 1] = self.view_width / (2.0 * numpy.tan(self.view_fov * numpy.pi / 360.0))
+        self.calibration = calibration
+
+        self.distCoeffs = numpy.array([0, 0, 0, 0], dtype=numpy.float)
+        self.rotVector = numpy.array([0], dtype=numpy.float)
+        self.transVector = numpy.array([0], dtype=numpy.float)
+        self.end_point_3d = numpy.array([0, 0, 1000], dtype=numpy.float)
+        self.end_point_2d = numpy.array([0, 0], dtype=numpy.float)
+
         # load the image, clone it, and setup the mouse callback function.
         clone = cv_img.copy()
         gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
         self.image = cv_img
         cv2.namedWindow("Image Window")
         cv2.setMouseCallback("Image Window", self.shape_selection)
+        cv2.namedWindow("3D Window")
+        cv_img_3D = numpy.array([0, 0, 1000], dtype=numpy.float)
 
         # Press r to reset the cropping region
         if (cv2.waitKey(1) & 0xFF) == ord("r"):
@@ -95,7 +119,10 @@ class Point_Projection:
             for pt in zip(*loc[::-1]):
                 cv2.rectangle(cv_img, pt, (pt[0] + self.tW, pt[1] + self.tH), (0, 0, 255), 2)
 
+        if self.project_points is True:
+            cv2.projectPoints(cv_img_3D, self.rotVector, self.transVector, self.calibration, self.distCoeffs, cv_img)
         cv2.imshow("Image Window", cv_img)
+        cv2.imshow("3D Window", cv_img_3D)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_img, "bgr8"))
