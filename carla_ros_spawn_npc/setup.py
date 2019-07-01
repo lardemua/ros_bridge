5a085c65a6443e7cfcc16ef825ab0b38bc338827
Pedro Silva <silva.mfpedro@gmail.com>
2019-07-01 16:29:58 +0100

    Added new catkin package to spawn npc objects into the simulation.

--- carla_ros_spawn_npc/setup.py
+++ carla_ros_spawn_npc/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['carla_ros_control_npc'],
+    packages=['carla_ros_spawn_npc'],
     package_dir={'': 'src'}
 )
 
--- carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_shape_selection.py
+++ carla_ros_template_matching/src/carla_ros_template_matching/carla_ros_shape_selection.py
@@ -63,10 +63,10 @@ class Shape_Selection:
         except CvBridgeError as e:
             print(e)
 
-        # client = carla.Client('127.0.0.1', 2000)
-        # client.set_timeout(2000)
-        # world = client.get_world()
-        # blueprints = world.get_blueprint_library().filter('vehicle.*')
+        client = carla.Client('127.0.0.1', 2000)
+        client.set_timeout(2000)
+        world = client.get_world()
+        blueprints = world.get_blueprint_library().filter('vehicle.*')
 
         # load the image, clone it, and setup the mouse callback function.
         clone = cv_img.copy()
@@ -74,8 +74,8 @@ class Shape_Selection:
         self.image = cv_img
         cv2.namedWindow("Image Window")
         cv2.setMouseCallback("Image Window", self.shape_selection)
-        print("I AM HERE")
-        cv2.waitKey(100)
+        # print("I AM HERE")
+        cv2.waitKey(1)
 
         # Press r to reset the cropping region
         if (cv2.waitKey(1) & 0xFF) == ord("r"):
