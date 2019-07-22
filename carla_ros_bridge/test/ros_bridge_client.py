2965df01adb5e00856e8b2be0ece3c3d486ea2a0
Pedro Silva <silva.mfpedro@gmail.com>
2019-07-22 22:19:03 +0100

    Changes in carla_ros_bridge test client module.

--- carla_ros_bridge/test/ros_bridge_client.py
+++ carla_ros_bridge/test/ros_bridge_client.py
@@ -8,10 +8,12 @@ import sys
 import unittest
 from carla_ros_bridge.client import main
 
-## A sample python unit test
+
+# A sample python unit test
 class TestPublishedTopics(unittest.TestCase):
     def test_camera_topics(self):
         self.assertEquals(1, 1, "1!=1")
+
 
 if __name__ == '__main__':
     rostest.rosrun(__name__, 'test_published_topics', TestPublishedTopics)
